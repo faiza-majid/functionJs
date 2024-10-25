8299bfe57953356125137c93780941e2f433db74
Faiza <faizamajid876@gmail.com>
2024-10-25 23:52:22 +0500

    function

---
+++ script.js
@@ -0,0 +1,21 @@
+// function functionName() {
+//     console.log("We are learning js");
+//     console.log("Function in js");
+// }
+// functionName();
+
+// function definition;
+// function functionName(js){ // Parameter
+//     console.log(js);
+
+// }
+// functionName("I love js"); // Argument
+
+function sum(x,y){
+    s = x+y;
+    console.log("before return");
+    return s;
+    console.log("after return");
+}
+let values = sum(3,4);
+console.log(values);
